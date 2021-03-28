9133de3d00484122ee4ccb7436dfc87f3f72edf0
Duvvvy <ryanbircham@gmail.com>
2021-03-29 12:13:55 +1300

    Spring init

---
+++ src/main/java/com/ryanbircham/vectorcrudapp/VectorcrudappApplication.java
@@ -0,0 +1,13 @@
+package com.ryanbircham.vectorcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorcrudappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VectorcrudappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ryanbircham/vectorcrudapp/VectorcrudappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanbircham.vectorcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorcrudappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
