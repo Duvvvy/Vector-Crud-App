67773a6bba588102f8f282e2d03e1c0fdc77392e
Duvvvy <ryanbircham@gmail.com>
2021-03-29 12:27:45 +1300

    Setup controller with repo

--- src/main/java/com/ryanbircham/vectorcrudapp/controllers/UserController.java
+++ src/main/java/com/ryanbircham/vectorcrudapp/controllers/UserController.java
@@ -1,4 +1,15 @@
 package com.ryanbircham.vectorcrudapp.controllers;
 
+import com.ryanbircham.vectorcrudapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
 public class UserController {
+    @Autowired
+    UserRepository UserRepository;
+
+    
 }
